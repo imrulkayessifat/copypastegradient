da3f9f3a3eae394f6f8c80b6e3f2c95089f11930
imrulkayessifat <kayesimrul224@gmail.com>
2024-03-18 00:35:38 +0600

    add

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className='mx-auto px-10 w-full mt-24'>
             <div className='flex items-center justify-center'>
-                <div className='flex flex-col items-center gap-5'>
+                <div className='flex flex-col items-center gap-8'>
                     <h1 className='font-bold text-center text-6xl'>Copy Paste Gradient</h1>
                     <p
                         className='dark:bg-[#0f0f0f]  px-10 py-2 bg-white border-gradient'
